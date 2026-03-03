e5d5bc6cf34ddc4909198ee9a75e0312564f66eb
StesNiash <111519914+StesNiash@users.noreply.github.com>
2026-03-03 07:13:41 +0300

    Нууу короче оружие теперь стреляет нет проблем с углом

--- Mindustry-master/core/src/mindustry/logic/LReadable.java
+++
@@ -1,6 +0,0 @@
-package mindustry.logic;
-
-public interface LReadable{
-    boolean readable(LExecutor exec);
-    void read(LVar position, LVar output);
-}
--- Mindustry-master/core/src/mindustry/logic/LWritable.java
+++
@@ -1,6 +0,0 @@
-package mindustry.logic;
-
-public interface LWritable{
-    boolean writable(LExecutor exec);
-    void write(LVar position, LVar value);
-}
